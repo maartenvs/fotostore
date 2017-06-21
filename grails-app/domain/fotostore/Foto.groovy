9badd3789733bea1802c8798cbbbe68d541f860c
Maarten van Schouwen <maarten@west.nl>
2017-06-22 00:29:17 +0200

    Adding image data to the Foto domain class.

--- grails-app/domain/fotostore/Foto.groovy
+++ grails-app/domain/fotostore/Foto.groovy
@@ -3,8 +3,10 @@ package fotostore
 class Foto {
 
     String type
+    byte[] image
 
     static constraints = {
         type nullable: false, blank: false
+        image nullable: false, maxSize: 1024 * 1024 * 20
     }
 }
